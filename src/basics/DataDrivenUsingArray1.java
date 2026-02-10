4a1df66f93fcc7ba8e54974456ff432417733f7d
shreyvyas <shreyvyas@gmail.com>
2026-02-10 22:54:22 +0530

    datadriven using 2-d array -1

--- src/basics/DataDrivenUsingArray1.java
+++ src/basics/DataDrivenUsingArray1.java
@@ -6,7 +6,7 @@ public class DataDrivenUsingArray1 {
 		
 		String[][] testData = {{"username1", "password1"}, {"username2", "password2"}};
 		
-		readTestData(testData);
+		readTestData2(testData);
 
 	}
 	
@@ -23,6 +23,34 @@ public class DataDrivenUsingArray1 {
 			}
 			
 		}
+		
+	}
+	
+	static void readTestData1(String[][] arr) {
+		
+		for(int i=0; i<arr.length; i++) {
+			
+			System.out.println(arr[i][0] + " " + arr[i][1]);
+			
+		}
+		
+	}
+	
+	static void readTestData2(String[][] arr) {
+		
+//		String username = arr[i][0];
+//		String password = arr[i][1];
+		
+		for(int i=0; i<arr.length; i++) {
+			
+			String username = arr[i][0];
+			String password = arr[i][1];
+			
+			System.out.println(username);
+			System.out.println(password);
+			
+		}
+		
 		
 	}
 
